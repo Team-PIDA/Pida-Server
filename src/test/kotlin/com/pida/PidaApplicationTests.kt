9af66ca5f312113b2bed29113581d811a7448004
ybchar <uiurihappy@naver.com>
2025-03-06 23:08:20 +0900

    init Project

---
+++ src/test/kotlin/com/pida/PidaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pida
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PidaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
