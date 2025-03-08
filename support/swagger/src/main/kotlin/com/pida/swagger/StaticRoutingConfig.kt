c107cc6eb5efbf477212e060352389cf3ba3567f
ybchar <uiurihappy@naver.com>
2025-03-08 13:21:08 +0900

    chore: swagger base

--- support/swagger/src/main/kotlin/com/pida/swagger/StaticRoutingConfig.kt
+++ support/swagger/src/main/kotlin/com/pida/swagger/StaticRoutingConfig.kt
@@ -1,4 +1,4 @@
-package com.partimestudy.swagger
+package com.pida.swagger
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
