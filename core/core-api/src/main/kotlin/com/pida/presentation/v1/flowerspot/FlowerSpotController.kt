d562d94bdf47cd0251805fec6fb3aa8748f1249d
yb__char <68099546+char-yb@users.noreply.github.com>
2025-03-20 22:34:28 +0900

    feat: find one flower spot (#4)

--- core/core-api/src/main/kotlin/com/pida/presentation/v1/flowerspot/FlowerSpotController.kt
+++ core/core-api/src/main/kotlin/com/pida/presentation/v1/flowerspot/FlowerSpotController.kt
@@ -4,10 +4,12 @@ import com.pida.flowerspot.FlowerSpotService
 import com.pida.flowerspot.Region
 import com.pida.presentation.v1.annotation.ApiV1Controller
 import com.pida.presentation.v1.flowerspot.response.FlowerSpotAllResponse
+import com.pida.presentation.v1.flowerspot.response.FlowerSpotDetails
 import com.pida.presentation.v1.flowerspot.response.FlowerSpotResponseDto
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 
 @Tag(name = "1. Flower Spot", description = "벚꽃 장소 관련 API")
@@ -23,5 +25,14 @@ class FlowerSpotController(
         val flowerSpots = flowerSpotService.findAllFlowerSpot(region)
         val responseDtoList = flowerSpots.map { FlowerSpotResponseDto.from(it) }
         return FlowerSpotAllResponse.of(responseDtoList)
+    }
+
+    @Operation(summary = "벚꽃 장소 상세 조회", description = "벚꽃 장소를 조회합니다.")
+    @GetMapping("/flower-spot/{spotId}")
+    suspend fun flowerSpotFindOne(
+        @PathVariable spotId: Long,
+    ): FlowerSpotDetails {
+        val flowerSpot = flowerSpotService.findOneFlowerSpot(spotId)
+        return FlowerSpotDetails.of(flowerSpot)
     }
 }
---
+++ core/core-api/src/main/kotlin/com/pida/presentation/v1/flowerspot/response/FlowerSpotDetails.kt
@@ -0,0 +1,67 @@
+package com.pida.presentation.v1.flowerspot.response
+
+import com.pida.flowerspot.FlowerSpot
+import com.pida.flowerspot.GeoJson
+import com.pida.flowerspot.Region
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+
+data class FlowerSpotDetails(
+    @Schema(description = "장소 ID", example = "1")
+    val id: Long,
+    @Schema(description = "주소", example = "서울특별시 강남구 수서동")
+    val address: String?,
+    @Schema(description = "최근 방문 횟수", example = "5")
+    val recentlyVisitedCount: Int = 0,
+    @Schema(description = "도로명", example = "밤고개1길")
+    val streetName: String,
+    @Schema(description = "행정동", example = "수서동")
+    val district: String?,
+    @Schema(description = "설명", example = "벚꽃이 예쁜 길")
+    val description: String?,
+    @Schema(
+        description = "라인 정보 (GeoJson)",
+        example = """
+        {
+          "type": "LineString",
+          "coordinates": [
+            [127.10079, 37.48809],
+            [127.10116, 37.48825],
+            [127.10221, 37.48852],
+            [127.10534, 37.48943]
+          ]
+        }
+    """,
+    )
+    val geom: GeoJson, // LineString GeoJson
+    @Schema(
+        description = "핀포인트 정보 (GeoJson)",
+        example = """
+        {
+          "type": "Point",
+          "coordinates": [127.10317, 37.48881]
+        }
+    """,
+    )
+    val pinPoint: GeoJson, // Point GeoJson
+    @Schema(description = "지역", example = "SEOUL")
+    val region: Region,
+    @Schema(description = "삭제 일자", example = "2025-04-01T00:00:00")
+    val deletedAt: LocalDateTime?,
+) {
+    companion object {
+        fun of(flowerSpot: FlowerSpot) =
+            FlowerSpotDetails(
+                id = flowerSpot.id,
+                address = flowerSpot.address,
+                recentlyVisitedCount = 0, // TODO: 사용자 방문 횟수
+                streetName = flowerSpot.streetName,
+                district = flowerSpot.district,
+                description = flowerSpot.description,
+                geom = flowerSpot.geom,
+                pinPoint = flowerSpot.pinPoint,
+                region = flowerSpot.region,
+                deletedAt = flowerSpot.deletedAt,
+            )
+    }
+}
