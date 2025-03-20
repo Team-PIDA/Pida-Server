11a12024c4096c67d63314f2975660c9febcb5e9
ybchar <uiurihappy@naver.com>
2025-03-20 18:00:43 +0900

    refactor: tx extension function

--- storage/db-core/src/main/kotlin/com/pida/storage/db/core/flowerspot/FlowerSpotCoreRepository.kt
+++ storage/db-core/src/main/kotlin/com/pida/storage/db/core/flowerspot/FlowerSpotCoreRepository.kt
@@ -3,13 +3,14 @@ package com.pida.storage.db.core.flowerspot
 import com.pida.flowerspot.FlowerSpot
 import com.pida.flowerspot.FlowerSpotRepository
 import com.pida.storage.db.core.support.findByIdAndDeletedAtIsNullOrElseThrow
-import com.pida.support.tx.TxAdvice
+import com.pida.support.tx.TransactionTemplates
+import com.pida.support.tx.coExecute
 import org.springframework.stereotype.Repository
 
 @Repository
 class FlowerSpotCoreRepository(
     private val flowerSpotJpaRepository: FlowerSpotJpaRepository,
-    private val txAdvice: TxAdvice,
+    private val tx: TransactionTemplates,
 ) : FlowerSpotRepository {
     override fun findBy(spotId: Long): FlowerSpot =
         flowerSpotJpaRepository
@@ -17,7 +18,7 @@ class FlowerSpotCoreRepository(
             .toFlowerSpot()
 
     override suspend fun findAll(): List<FlowerSpot> =
-        txAdvice.coReadOnly {
+        tx.reader.coExecute {
             flowerSpotJpaRepository
                 .findByDeletedAtIsNull()
                 .map { it.toFlowerSpot() }
