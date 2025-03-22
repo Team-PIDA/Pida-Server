fe55ced746dc4502d3acfc675588d235416ee265
ybchar <uiurihappy@naver.com>
2025-03-22 16:47:17 +0900

    feat: user Repository CRUD

--- storage/db-core/src/main/kotlin/com/pida/storage/db/core/user/UserCoreRepository.kt
+++ storage/db-core/src/main/kotlin/com/pida/storage/db/core/user/UserCoreRepository.kt
@@ -1,8 +1,17 @@
 package com.pida.storage.db.core.user
 
+import com.pida.storage.db.core.support.findByIdAndDeletedAtIsNullOrElseThrow
+import com.pida.support.error.ErrorException
+import com.pida.support.error.ErrorType
 import com.pida.support.tx.TransactionTemplates
 import com.pida.support.tx.TxAdvice
+import com.pida.support.tx.coExecute
+import com.pida.user.NewUser
+import com.pida.user.NewUserKey
+import com.pida.user.User
+import com.pida.user.UserProfile
 import com.pida.user.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -10,5 +19,104 @@ class UserCoreRepository(
     private val userJpaRepository: UserJpaRepository,
     private val tx: TransactionTemplates,
     private val txAdvice: TxAdvice,
-): UserRepository {
+) : UserRepository {
+    override fun create(
+        newUser: NewUser,
+        newUserKey: NewUserKey,
+    ): User =
+        txAdvice.write {
+            return@write userJpaRepository.save(UserEntity(newUser, newUserKey)).toUser()
+        }
+
+    override suspend fun readUserById(id: Long): User? =
+        txAdvice.readOnly {
+            userJpaRepository.findByIdOrNull(id)?.toUser()
+        }
+
+    override suspend fun readByNameAndPhone(
+        name: String,
+        phone: String,
+    ): UserProfile =
+        tx.reader.coExecute {
+            userJpaRepository
+                .findByNameAndPhoneAndDeletedAtIsNull(name, phone)
+                ?.toProfile()
+                ?: throw ErrorException(ErrorType.NOT_FOUND_DATA)
+        }
+
+    override suspend fun readByUserIdOrNull(id: Long): UserProfile? =
+        tx.reader.coExecute {
+            userJpaRepository.findByIdAndDeletedAtIsNull(id)?.toProfile()
+        }
+
+    override suspend fun readByPhoneNumber(phoneNumber: String): UserProfile? =
+        tx.reader.coExecute {
+            userJpaRepository.findByPhone(phoneNumber)?.toProfile()
+        }
+
+    override suspend fun readAllByUserIds(userIds: List<Long>): List<UserProfile> =
+        tx.reader.coExecute {
+            userJpaRepository.findAllByIdIn(userIds).map { it.toProfile() }
+        }
+
+    override suspend fun readByUserKey(userKey: String): UserProfile =
+        tx.reader.coExecute {
+            userJpaRepository.findByUserKey(userKey)?.toProfile()
+                ?: throw ErrorException(ErrorType.NOT_FOUND_DATA)
+        }
+
+    override suspend fun readByUserId(id: Long): UserProfile =
+        tx.reader.coExecute {
+            userJpaRepository.findByIdAndDeletedAtIsNullOrElseThrow(id).toProfile()
+        }
+
+    override suspend fun existsByEmailOrPhone(
+        email: String,
+        phone: String,
+    ): Boolean =
+        tx.reader.coExecute {
+            userJpaRepository.existsByEmailAndPhoneAndDeletedAtIsNull(email, phone)
+        }
+
+    override suspend fun existsByEmail(email: String): Boolean =
+        tx.reader.coExecute {
+            userJpaRepository.existsByEmailAndDeletedAtIsNull(email)
+        }
+
+    override suspend fun existsByPhone(phone: String): Boolean =
+        tx.reader.coExecute {
+            userJpaRepository.existsByPhoneAndDeletedAtIsNull(phone)
+        }
+
+    override suspend fun updateNickname(
+        userKey: String,
+        nickname: String,
+    ): UserProfile =
+        tx.writer.coExecute {
+            val user = userJpaRepository.findByUserKey(userKey) ?: throw ErrorException(ErrorType.NOT_FOUND_DATA)
+            user.updateNickname(nickname)
+            return@coExecute user.toProfile()
+        }
+
+    override suspend fun updatePhone(
+        userKey: String,
+        phone: String,
+    ): UserProfile =
+        tx.writer.coExecute {
+            val user = userJpaRepository.findByUserKey(userKey) ?: throw ErrorException(ErrorType.NOT_FOUND_DATA)
+            user.updatePhone(phone)
+            return@coExecute user.toProfile()
+        }
+
+    override suspend fun updateEmail(
+        userKey: String,
+        email: String,
+    ): UserProfile =
+        tx.writer.coExecute {
+            val user = userJpaRepository.findByUserKey(userKey) ?: throw ErrorException(ErrorType.NOT_FOUND_DATA)
+            user.updateEmail(email)
+            return@coExecute user.toProfile()
+        }
+
+    override suspend fun delete(userKey: String) = tx.writer.coExecute { userJpaRepository.deleteByUserKey(userKey) }
 }
--- storage/db-core/src/main/kotlin/com/pida/storage/db/core/user/UserEntity.kt
+++ storage/db-core/src/main/kotlin/com/pida/storage/db/core/user/UserEntity.kt
@@ -2,7 +2,15 @@ package com.pida.storage.db.core.user
 
 import com.pida.storage.db.core.support.BaseEntity
 import com.pida.user.Gender
-import jakarta.persistence.*
+import com.pida.user.NewUser
+import com.pida.user.NewUserKey
+import com.pida.user.User
+import com.pida.user.UserProfile
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Table
 import java.time.LocalDate
 
 @Entity
@@ -10,11 +18,56 @@ import java.time.LocalDate
 class UserEntity(
     @Column(name = "user_key")
     val userKey: String,
-    val name: String,
-    val email: String,
-    val phone: String,
+    var name: String,
+    var nickname: String,
+    var email: String,
+    var phone: String,
     @Enumerated(value = EnumType.STRING)
     @Column(columnDefinition = "varchar(50)")
     val gender: Gender,
     var birth: LocalDate,
-): BaseEntity()
+) : BaseEntity() {
+    constructor(
+        newUser: NewUser,
+        newUserKey: NewUserKey,
+    ) : this(
+        userKey = newUserKey.key,
+        email = newUser.email,
+        name = newUser.name,
+        nickname = newUser.nickname,
+        phone = newUser.phone,
+        gender = newUser.gender,
+        birth = newUser.birth,
+    )
+
+    fun toUser(): User =
+        User(
+            id = id!!,
+            key = userKey,
+        )
+
+    fun toProfile(): UserProfile =
+        UserProfile(
+            id = id!!,
+            key = userKey,
+            email = email,
+            name = name,
+            nickname = nickname,
+            phone = phone,
+            gender = gender,
+            birth = birth,
+            createdAt = createdAt,
+        )
+
+    fun updateNickname(nickname: String) {
+        this.nickname = nickname
+    }
+
+    fun updatePhone(phone: String) {
+        this.phone = phone
+    }
+
+    fun updateEmail(email: String) {
+        this.email = email
+    }
+}
