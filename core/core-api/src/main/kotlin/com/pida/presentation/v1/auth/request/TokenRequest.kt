3d0306904ee859e57581ede8bbe746e9310645e6
yb__char <68099546+char-yb@users.noreply.github.com>
2025-03-23 17:35:36 +0900

    [Feature] Social login (Apple & Kakao) (#12)
    * feat: social login
    
    * fix: social-login request swagger

--- core/core-api/src/main/kotlin/com/pida/presentation/v1/auth/request/TokenRequest.kt
+++ core/core-api/src/main/kotlin/com/pida/presentation/v1/auth/request/TokenRequest.kt
@@ -1,5 +1,9 @@
 package com.pida.presentation.v1.auth.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "토큰 요청 Json")
 data class TokenRequest(
+    @Schema(description = "토큰", example = "id_token or access_token")
     val token: String,
 )
