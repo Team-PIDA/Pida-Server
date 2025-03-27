d63f40cb0e23c134d28898e99e56269c4e89d868
yb__char <68099546+char-yb@users.noreply.github.com>
2025-03-27 22:55:41 +0900

    [Feature] Add UserProfile (#31)
    * feat: Add UserProfile
    
    * fix: user parameter hidden

--- core/core-api/src/main/kotlin/com/pida/presentation/v1/user/UserController.kt
+++ core/core-api/src/main/kotlin/com/pida/presentation/v1/user/UserController.kt
@@ -1,8 +1,25 @@
 package com.pida.presentation.v1.user
 
 import com.pida.presentation.v1.annotation.ApiV1Controller
+import com.pida.presentation.v1.user.response.UserProfileResponse
+import com.pida.user.User
+import com.pida.user.UserService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
 
 @Tag(name = "2. User", description = "유저 관련 API")
 @ApiV1Controller
-class UserController
+class UserController(
+    private val userService: UserService,
+) {
+    @Operation(summary = "내 정보 조회", description = "내 정보를 조회합니다.")
+    @GetMapping("/users/me")
+    suspend fun me(
+        @Parameter(hidden = true, required = false) user: User,
+    ): UserProfileResponse {
+        val userProfile = userService.getProfile(user.id)
+        return UserProfileResponse.of(userProfile)
+    }
+}
