dae11be7fd03f6f61a7ffedcd67c4529c9bb82f9
yb__char <68099546+char-yb@users.noreply.github.com>
2025-03-28 17:43:55 +0900

    [Feature] Modify User nickname (#39)
    * feat: Update user nickname
    
    * refactor: AppleProperties

--- clients/oauth-client/src/main/kotlin/com/pida/client/oauth/AppleApi.kt
+++ clients/oauth-client/src/main/kotlin/com/pida/client/oauth/AppleApi.kt
@@ -1,11 +1,10 @@
 package com.pida.client.oauth
 
-import com.pida.client.oauth.AppleClient.Companion.APPLE_URI
 import com.pida.client.oauth.response.ApplePublicKeysResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient(name = "apple-auth-api", url = APPLE_URI)
+@FeignClient(name = "apple-auth-api", url = "https://appleid.apple.com")
 internal interface AppleApi {
     @GetMapping("/auth/keys")
     fun getApplePublicKeys(): ApplePublicKeysResponse
