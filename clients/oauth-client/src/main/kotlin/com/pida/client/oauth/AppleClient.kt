2bc88920ca91ea2dbc894c3084defae29abc1ca7
ybchar <uiurihappy@naver.com>
2025-03-28 17:40:07 +0900

    refactor: AppleProperties

--- clients/oauth-client/src/main/kotlin/com/pida/client/oauth/AppleClient.kt
+++ clients/oauth-client/src/main/kotlin/com/pida/client/oauth/AppleClient.kt
@@ -17,13 +17,9 @@ import java.util.Date
 @Component
 class AppleClient internal constructor(
     private val appleApi: AppleApi,
+    private val appleProperties: AppleProperties,
     private val objectMapper: ObjectMapper,
 ) {
-    companion object {
-        const val APPLE_BUNDLE_ID = "com.pida.me.ios.dev"
-        const val APPLE_URI = "https://appleid.apple.com"
-    }
-
     fun getUserInfo(token: String): AppleClientResult {
         val signedJWT: SignedJWT
         val jwtClaims: JWTClaimsSet
@@ -60,10 +56,10 @@ class AppleClient internal constructor(
         val currentDate = Date(System.currentTimeMillis())
         // audience 확인 부분 개선
         val bundleId = jwtClaims.audience.firstOrNull()
-        if (bundleId != APPLE_BUNDLE_ID) return false
+        if (bundleId != appleProperties.bundleId) return false
 
         val appleUrl = jwtClaims.issuer
-        return currentDate.before(jwtClaims.expirationTime) && appleUrl == APPLE_URI
+        return currentDate.before(jwtClaims.expirationTime) && appleUrl == "https://appleid.apple.com"
     }
 
     private fun isSignatureValid(signedJWT: SignedJWT): Boolean {
