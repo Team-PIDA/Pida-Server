53419e9c9d806ba72a810b5b7f42f5fb40fe3d35
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-01-19 14:28:25 +0900

    feat: add map client module and implement kakao search api

---
+++ pida-clients/map-client/src/main/kotlin/com/pida/client/map/MapFeignConfig.kt
@@ -0,0 +1,8 @@
+package com.pida.client.map
+
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableFeignClients
+internal class MapFeignConfig
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,7 @@ include(
     "pida-clients:aws-client",
     "pida-clients:notification",
     "pida-clients:oauth-client",
+    "pida-clients:map-client",
 )
 
 include(
