cc1bc2ef1c9d86e2ae97220c2bbd19a8352a198e
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-01-30 11:10:52 +0900

    [Feature] Add District data model for geographic representation

---
+++ pida-core/core-domain/src/main/kotlin/com/pida/landmark/District.kt
@@ -0,0 +1,13 @@
+package com.pida.landmark
+
+import com.pida.support.geo.GeoJson
+import com.pida.support.geo.Region
+
+data class District(
+    val id: Long,
+    val sido: Region,
+    val sigungu: String,
+    val eupmyeondong: String?,
+    val ri: String?,
+    val pinPoint: GeoJson,
+)
