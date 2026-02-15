7d46d85c8eb253c04addac25c836c0e42b2acc4e
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-02-15 17:35:31 +0900

    chore: add Sentry fingerprint callback for custom error handling

--- pida-supports/monitoring/build.gradle.kts
+++ pida-supports/monitoring/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.micrometer.registry.prometheus)
     implementation(libs.sentry.spring.boot.starter.jakarta)
+    implementation(project(":pida-core:core-domain"))
 }
