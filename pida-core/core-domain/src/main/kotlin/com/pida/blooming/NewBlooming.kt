a32b8e6a5746238677e877c927d63f3def3ae9a3
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-06 15:26:58 +0900

    fix: Add overridden flowerSpotId to use in BloomedSpotNotificationEventListener

--- pida-core/core-domain/src/main/kotlin/com/pida/blooming/NewBlooming.kt
+++ pida-core/core-domain/src/main/kotlin/com/pida/blooming/NewBlooming.kt
@@ -2,17 +2,19 @@ package com.pida.blooming
 
 sealed class NewBlooming {
     abstract val userId: Long
+    abstract val flowerSpotId: Long
     abstract val status: BloomingStatus
 
     data class FlowerSpot(
         override val userId: Long,
-        val flowerSpotId: Long,
+        override val flowerSpotId: Long,
         override val status: BloomingStatus,
     ) : NewBlooming()
 
     data class FlowerEvent(
         override val userId: Long,
-        val flowerEventId: Long,
+        override val flowerSpotId: Long,
         override val status: BloomingStatus,
+        val flowerEventId: Long,
     ) : NewBlooming()
 }
