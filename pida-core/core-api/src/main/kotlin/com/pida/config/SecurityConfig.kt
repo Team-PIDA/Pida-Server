8b408b4f3305776bddf0dd86c2d52452937b315a
char-yb <uiurihappy@naver.com>
2026-03-12 22:37:20 +0900

    fix: security config path

--- pida-core/core-api/src/main/kotlin/com/pida/config/SecurityConfig.kt
+++ pida-core/core-api/src/main/kotlin/com/pida/config/SecurityConfig.kt
@@ -134,7 +134,7 @@ class SecurityConfig(
                     "/api/v1/flower-spot/**",
                     "/api/v1/auth/**",
                     "/api/v1/flower-spot",
-                    "/api/v1/categories/**",
+                    "/api/v2/categories/**",
                     "/api/v1/auth",
                     "/api/v1/blooming/{spotId}/details",
                     "/api/v1/places/search",
