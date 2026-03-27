8365c8239f07a98db21764fb64e94aff35259c9b
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-27 16:39:10 +0900

    feat: temporally set as blocking s3client

--- pida-clients/aws-client/src/main/kotlin/com/pida/client/aws/image/ImageS3Processor.kt
+++ pida-clients/aws-client/src/main/kotlin/com/pida/client/aws/image/ImageS3Processor.kt
@@ -93,13 +93,24 @@ class ImageS3Processor(
     ): S3ImageInfo? {
         val imageFilePath = imageFileConstructor.imageFilePath(prefix, prefixId)
 
-        return awsS3AsyncClient
-            .listObjects(
+        return awsS3Client
+            .getBucketListObjects(
                 bucketName = awsProperties.s3.bucket,
                 filePath = imageFilePath,
-            ).filterNot { it.key().endsWith("/") }
+            ).contents()
+            .orEmpty()
+            .asSequence()
+            .filterNot { it.key().endsWith("/") }
             .maxByOrNull(S3Object::lastModified)
             ?.toImageInfo(imageFilePath, Duration.ofSeconds(30))
+
+//        return awsS3AsyncClient
+//            .listObjects(
+//                bucketName = awsProperties.s3.bucket,
+//                filePath = imageFilePath,
+//            ).filterNot { it.key().endsWith("/") }
+//            .maxByOrNull(S3Object::lastModified)
+//            ?.toImageInfo(imageFilePath, Duration.ofSeconds(30))
     }
 
     private fun generateGetUrl(
