a71587dd3cefcb59dfa88ac8c0994c85d4fd030c
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-27 16:49:20 +0900

    feat: add logs to check querying time

--- pida-core/core-domain/src/main/kotlin/com/pida/flowerspot/FlowerSpotFacade.kt
+++ pida-core/core-domain/src/main/kotlin/com/pida/flowerspot/FlowerSpotFacade.kt
@@ -47,12 +47,16 @@ class FlowerSpotFacade(
         location: FlowerSpotLocation,
     ): List<FlowerSpotDetails> =
         coroutineScope {
+            val point1 = System.currentTimeMillis()
             val flowerSpots = flowerSpotService.readAllFlowerSpot(region, location)
+            logger.info("flowerSpots querying time ${System.currentTimeMillis() - point1}ms")
             if (flowerSpots.isEmpty()) return@coroutineScope emptyList()
 
+            val point2 = System.currentTimeMillis()
             val bloomingDeferred = async { bloomingService.recentlyBloomingBySpotIds(flowerSpots.map { it.id }) }
+            logger.info("bloomingDeferred querying time ${System.currentTimeMillis() - point2}ms")
 
-            val startTime = System.currentTimeMillis()
+            val point3 = System.currentTimeMillis()
             val previewDeferred =
                 flowerSpots.map { spot ->
                     async {
@@ -63,10 +67,12 @@ class FlowerSpotFacade(
                             )
                     }
                 }
-            logger.info("Preview image fetch initiated for ${flowerSpots.size} spots in ${System.currentTimeMillis() - startTime}ms")
+            logger.info("previewDeferred querying time ${System.currentTimeMillis() - point3}ms")
 
+            val point4 = System.currentTimeMillis()
             val bloomingBySpotId = bloomingDeferred.await().groupBy { it.flowerSpotId }
             val previewBySpotId = previewDeferred.associate { it.await() }
+            logger.info("grouping time ${System.currentTimeMillis() - point4}ms")
 
             flowerSpots.map { flowerSpot ->
                 FlowerSpotDetails.of(
