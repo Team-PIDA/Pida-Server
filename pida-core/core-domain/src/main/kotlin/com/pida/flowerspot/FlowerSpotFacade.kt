a841c8270f7d57a8dda37799646587aeabf3a37e
leehaneum <leehaneum160924@kyonggi.ac.kr>
2026-03-27 16:28:29 +0900

    feat: add log temporally

--- pida-core/core-domain/src/main/kotlin/com/pida/flowerspot/FlowerSpotFacade.kt
+++ pida-core/core-domain/src/main/kotlin/com/pida/flowerspot/FlowerSpotFacade.kt
@@ -1,11 +1,9 @@
 package com.pida.flowerspot
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.pida.blooming.BloomingService
 import com.pida.support.aws.ImagePrefix
 import com.pida.support.aws.ImageS3Caller
-import com.pida.support.aws.S3ImageInfo
-import com.pida.support.cache.Cache
+import com.pida.support.extension.logger
 import com.pida.support.geo.Region
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -21,6 +19,8 @@ class FlowerSpotFacade(
         const val PREVIEW_IMAGE_KEY = "spot:preview"
         const val PREVIEW_IMAGE_TTL = 10L
     }
+
+    private val logger by logger()
 
     suspend fun readFlowerSpotDetails(spotId: Long): FlowerSpotDetails =
         coroutineScope {
@@ -51,10 +51,19 @@ class FlowerSpotFacade(
             if (flowerSpots.isEmpty()) return@coroutineScope emptyList()
 
             val bloomingDeferred = async { bloomingService.recentlyBloomingBySpotIds(flowerSpots.map { it.id }) }
+
+            val startTime = System.currentTimeMillis()
             val previewDeferred =
                 flowerSpots.map { spot ->
-                    async { spot.id to cachedPreviewImage(spot.id) }
+                    async {
+                        spot.id to
+                            imageS3Caller.getPreviewImage(
+                                prefix = ImagePrefix.FLOWERSPOT.value,
+                                prefixId = spot.id,
+                            )
+                    }
                 }
+            logger.info("Preview image fetch initiated for ${flowerSpots.size} spots in ${System.currentTimeMillis() - startTime}ms")
 
             val bloomingBySpotId = bloomingDeferred.await().groupBy { it.flowerSpotId }
             val previewBySpotId = previewDeferred.associate { it.await() }
@@ -68,15 +77,15 @@ class FlowerSpotFacade(
             }
         }
 
-    private suspend fun cachedPreviewImage(spotId: Long): S3ImageInfo? =
-        Cache.cache(
-            ttl = PREVIEW_IMAGE_TTL,
-            key = "$PREVIEW_IMAGE_KEY:$spotId",
-            typeReference = object : TypeReference<S3ImageInfo?>() {},
-        ) {
-            imageS3Caller.getPreviewImage(
-                prefix = ImagePrefix.FLOWERSPOT.value,
-                prefixId = spotId,
-            )
-        }
+//    private suspend fun cachedPreviewImage(spotId: Long): S3ImageInfo? =
+//        Cache.cache(
+//            ttl = PREVIEW_IMAGE_TTL,
+//            key = "$PREVIEW_IMAGE_KEY:$spotId",
+//            typeReference = object : TypeReference<S3ImageInfo?>() {},
+//        ) {
+//            imageS3Caller.getPreviewImage(
+//                prefix = ImagePrefix.FLOWERSPOT.value,
+//                prefixId = spotId,
+//            )
+//        }
 }
